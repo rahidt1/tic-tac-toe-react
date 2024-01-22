a529b2f1b26f14aad1273ccdcc33641749e5115d
Tanvir Ahmed Talukder <rahidt78@gmail.com>
2024-01-22 23:47:30 +0600

    Check for winner using winning combination.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,13 @@ import { useState } from "react";
 import { GameBoard } from "./components/GameBoard";
 import { Player } from "./components/Player";
 import { Log } from "./components/Log";
+import { WINNING_COMBINATIONS } from "./winning-combinations.js";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
 
 function derivePlayer(gameTurns) {
   let currentPlayer = "X";
@@ -15,6 +22,32 @@ function App() {
   const [gameTurns, setGameTurns] = useState([]);
   // const [activePlayer, setActivePlayer] = useState("X");
   const activePlayer = derivePlayer(gameTurns);
+
+  let gameBoard = initialGameBoard;
+
+  for (const turn of gameTurns) {
+    // prettier-ignore
+    const {square:{row,col},player} = turn
+
+    gameBoard[row][col] = player;
+  }
+
+  // Check for winner
+  // Check three symbols from board using different winning combination positions. If the symbols are same, we got our winner !
+
+  let winner;
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSymbol = gameBoard[combination[0].row][combination[0].col];
+    const secondSymbol = gameBoard[combination[1].row][combination[1].col];
+    const thirdSymbol = gameBoard[combination[2].row][combination[2].col];
+
+    if (
+      firstSymbol &&
+      firstSymbol === secondSymbol &&
+      firstSymbol === thirdSymbol
+    )
+      winner = firstSymbol;
+  }
 
   function handleSelectSquare(rowIndex, colIndex) {
     // setActivePlayer((curr) => (curr === "X" ? "O" : "X"));
@@ -45,7 +78,12 @@ function App() {
             isActive={activePlayer === "O"}
           />
         </ol>
-        <GameBoard turns={gameTurns} onSelectSquare={handleSelectSquare} />
+        {winner && <p>You won ! {winner}</p>}
+        <GameBoard
+          turns={gameTurns}
+          onSelectSquare={handleSelectSquare}
+          board={gameBoard}
+        />
       </div>
       <Log turns={gameTurns} />
     </main>
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,37 +1,7 @@
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
-
-export function GameBoard({ onSelectSquare, turns }) {
-  let gameBoard = initialGameBoard;
-
-  for (const turn of turns) {
-    // prettier-ignore
-    const {square:{row,col},player} = turn
-
-    gameBoard[row][col] = player;
-  }
-
-  /*
-  const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-  function handleSelectSquare(rowIndex, colIndex) {
-    setGameBoard((prevGameBoard) => {
-      //const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-      const updatedBoard = structuredClone(prevGameBoard); //Deep clone
-      updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-      return updatedBoard;
-    });
-
-    onSelectSquare();
-  }
-  */
-
+export function GameBoard({ onSelectSquare, board }) {
   return (
     <ol id="game-board">
-      {gameBoard.map((row, rowIndex) => (
+      {board.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
